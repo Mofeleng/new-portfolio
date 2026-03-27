b762bced5e1ab0ec1a8f70745d089e7ffd7d4fe5
Mofeleng <mofelengmokgope@gmail.com>
2026-03-27 23:23:57 +0200

    Created navbar and fixed tsconfig

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
+import Navbar from '@components/navbar'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Welcome to my MacFolio</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   )
 }
 
